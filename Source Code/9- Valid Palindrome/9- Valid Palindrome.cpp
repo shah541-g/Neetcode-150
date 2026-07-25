8be4e03c8172bc9dad17912e787b76a2ea6ad4f9
shah541-g <syedabdulalishah.786@gmail.com>
2026-07-25 19:04:34 +0500

    Starting 10th Neetcode Problem

--- Source Code/9- Valid Palindrome/9- Valid Palindrome.cpp
+++ Source Code/9- Valid Palindrome/9- Valid Palindrome.cpp
@@ -5,39 +5,66 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
-        
+  bool isPalindrome(string s)
+  {
+
+    for (auto &x : s)
+    {
+      x = tolower(x);
     }
+    int left = 0, right = s.length() - 1;
+    while (left < right)
+    {
+
+      while (!isalnum(s[left]))
+      {
+        left++;
+      }
+      while (!isalnum(s[right]))
+      {
+        right--;
+      }
+      if (s[left] != s[right])
+      {
+        return false;
+      }
+      left++, right--;
+    }
+    return true;
+  }
 };
 
-int main() {
-    Solution obj;
+int main()
+{
+  Solution obj;
 
-    vector<string> testCases = {
-        "A man, a plan, a canal: Panama", // true
-        "race a car",                     // false
-        " ",                              // true
-        "",                               // true
-        "0P",                             // false
-        "Madam",                          // true
-        "No 'x' in Nixon",                // true
-        "Was it a car or a cat I saw?",   // true
-        "hello",                          // false
-        "12321",                          // true
-        "1231",                           // false
-        ".,!@#",                          // true
-        "Able was I, ere I saw Elba"      // true
-    };
+  vector<string> testCases = {
+      "A man, a plan, a canal: Panama", // true
+      "race a car",                     // false
+      " ",                              // true
+      "",                               // true
+      "0P",                             // false
+      "Madam",                          // true
+      "No 'x' in Nixon",                // true
+      "Was it a car or a cat I saw?",   // true
+      "hello",                          // false
+      "12321",                          // true
+      "1231",                           // false
+      ".,!@#",                          // flase
+      "Able was I, ere I saw Elba"      // true
+  };
 
-    for (int i = 0; i < testCases.size(); i++) {
-        cout << "Test Case " << i + 1 << ": ";
+  for (int i = 0; i < testCases.size(); i++)
+  {
+    cout << "Test Case " << i + 1 << ": ";
 
-        bool result = obj.isPalindrome(testCases[i]);
+    bool result = obj.isPalindrome(testCases[i]);
 
-        cout << (result ? "true" : "false") << endl;
-    }
+    cout << (result ? "true" : "false") << endl;
+  }
 
-    return 0;
+  return 0;
 }
